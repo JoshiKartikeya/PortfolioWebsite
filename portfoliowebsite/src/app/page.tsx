bf867238ba799151416e80c2e7ed6e29ee690d55
Kartikeya Joshi <kartikeyajoshi30@gmail.com>
2025-06-14 13:04:47 +0530

    new changes

--- portfoliowebsite/src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-// import { Typography } from "@mui/material";
-import AllComponents from "@/components/AllComponents";
-
-export default function Home() {
-  return (<main>
-      <AllComponents /></main>
-  );
-}
